6dd5ef0460cdef4d7f07e714834921b678abbb4f
oshyho <realyuremi@gmail.com>
2023-01-26 07:50:10 +0900

    js 100제

--- js 31-40/js33.js
+++ js 31-40/js33.js
@@ -2,6 +2,7 @@
 
 // 한 줄에 여러개의 숫자가 입력되면, 역순으로 그 숫자들을 하나씩 출력하는 프로그램을 작성하시오.
 
-const str = '안녕하세요';
-const reversed = str.split('').reverse().join('');
-console.log(reversed);
+const str = '45 23 12 34 36 46 32';
+const reverse = (str) => str.split(' ').reverse().join(' ')
+
+console.log(reverse(str))
--- js 31-40/js34.js
+++ js 31-40/js34.js
@@ -1,35 +1,23 @@
-// // # 문제34 : sort 구현하기
-
-// 민주는 체육부장으로 체육시간이 되면 반 친구들이 제대로 키 순서대로 모였는지를 확인해야 한다. 그런데 요즘 민주는 그것이 너무 번거롭게 느껴져 한 번에 확인하고 싶어한다.
-
-// 민주를 위해 **키가 주어지면 순서대로 제대로 섰는지 확인하는 프로그램**을 작성해보자.
-// (키는 공백으로 구분하여 입력됩니다.)
-
-
-// const arr = [5, 4, 3, 2, 1];
-
-// const sorted = arr.sort(function (a, b) {
-//   for (let i = 0; i < arr.length; i++) {
-//     return a - b;
-//   }
-// });
-
-// for (let i = 0; i < arr.length; i++) {
-//   if (arr[i] == sorted) {
-//     console.log("true");
-//   } else {
-//     console.log("false");
-//   }
-// }
-
-
-const unsorted = '12 45 78'
-let sorted = "";
-
-sorted = unsorted.split(" ").sort().join(" ");
-  // console.log(typeof(sorted),sorted)
-if (unsorted === sorted) {
-  console.log("Yes");
-} else {
-  console.log("No");
+// let unsorted = [prompt()]
+let unsorted = prompt('공백으로 입력하세').split(' ');
+let sorted= () =>{
+  for(let i =0 ; i<unsorted.length; i++){
+    if(unsorted[i]>unsorted[i+1]){
+      continue;
+    }else{
+      sorted[i] = unsorted[i]
+    }
+  }
+  return sorted;
 }
+
+function makeSorted(input){
+  for(let i=0; i<unsorted.length; i++){
+    if(unsorted[i] === sorted[i]){
+
+    }
+  }
+
+}
+
+makeSorted('')
