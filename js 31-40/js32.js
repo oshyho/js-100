e17912d8c5fe9e47b0a90e4959e228933bb3f185
oshyho <realyuremi@gmail.com>
2023-02-15 07:19:31 +0900

    js 100제

--- js 31-40/js32.js
+++ js 31-40/js32.js
@@ -1,5 +1,5 @@
 const str = '안녕하세요, 모두들 좋은 아침입니다!'
 
-const answer = (str) => str.split(' ').length
+const strLength = (string) => string.split(' ').length
 
-console.log (answer(str))
+console.log(strLength(str))
--- js 31-40/js37.js
+++ js 31-40/js37.js
@@ -1,2 +1,5 @@
-const test  = 'hi'
-console.log(test);
+const test = '원범 원범 원범 혜원 혜원 유진'
+
+  for(let key in test){
+    console.log (test)
+  }
