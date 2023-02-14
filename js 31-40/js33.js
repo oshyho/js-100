860aa5514cc64b6b1d8849cf7509492dc8157d88
oshyho <realyuremi@gmail.com>
2023-02-15 07:19:16 +0900

    js 100제

--- js 31-40/js33.js
+++ js 31-40/js33.js
@@ -3,6 +3,3 @@
 // 한 줄에 여러개의 숫자가 입력되면, 역순으로 그 숫자들을 하나씩 출력하는 프로그램을 작성하시오.
 
 const str = '45 23 12 34 36 46 32';
-const reverse = (str) => str.split(' ').reverse().join(' ')
-
-console.log(reverse(str))
